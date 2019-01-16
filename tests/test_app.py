8bf3fed1eacd32d0e8c387ade435ea5fe98047f0
CrossroadsMan <crossroadsman@gmail.com>
2019-01-15 21:56:34 -0700

    Start building project layout

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+import unittest
+
+import todo.app
+
+
+class TestApp(unittest.TestCase):
+
+    def test_my_todos_renders_correct_template(self):
+        self.fail()
+
+
+
+
+# ------------------------
+
+if __name__ == '__main__':
+    unittest.main()
