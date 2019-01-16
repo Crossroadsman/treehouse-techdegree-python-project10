7cd9a68ef996592edd13739314fa3434aa428013
CrossroadsMan <crossroadsman@gmail.com>
2019-01-16 15:35:35 -0700

    Start writing tests

--- todo/tests/test_app.py
+++ todo/tests/test_app.py
@@ -7,19 +7,14 @@ from peewee import *
 import app
 
 
+VERBOSE = False
+
+
 class TestApp(unittest.TestCase):
-
-    def test_my_todos_renders_correct_template(self):
-
-        # Flask apps have a config attribute. This is a reference
-        # to a Config object (a dictionary-like object). This does
-        # not read in the values from our `config.py` file by default.
-        #
-        # create a temp database file
-        # (we get a file handle and a filename with full path)
-        # (note, the file handle is low-level so we need to use
-        # os.close() instead of <fh>.close() to close it)
-        temp_db_fh, app.app.config["DATABASE"] = tempfile.mkstemp()
+    
+    # Setup and Teardown
+    # ------------------
+    def setUp(self):
 
         # Set testing mode
         # (This disables error catching during request handling, so
@@ -29,41 +24,40 @@ class TestApp(unittest.TestCase):
         # Create a test client
         self.app = app.app.test_client()
 
-        # Initialise the DB
-        temp_db = SqliteDatabase(app.app.config["DATABASE"])
-        temp_db.connect()
-        temp_db.create_tables([], safe=True)
-        temp_db.close()
-        
-
+    # Tests
+    # -----
+    def test_my_todos_can_load_page(self):
         # response is a flask.wrappers.Response object
         response = self.app.get('/')
-        print("==== DEBUG response object ====")
-        print("response object")
-        print(type(response))
-        print(dir(response))
-        print("Interesting attributes:")
-        print(f"content_encoding: {response.content_encoding}")
-        print(f"data (byte string): {response.data}")
-        print(f"data type: {type(response.data)}")
-        print(f"get_data: {response.get_data()}")
-        print(f"status_code: {response.status_code}")
-        print(f"status_code type: {type(response.status_code)}")
-        print(f"json: {response.json}")
-        print(f"get_json: {response.get_json()}")
-        print(f"headers: {response.headers}")
-        print(f"headers type: {type(response.headers)}")
-        print(f"headers attributes: {dir(response.headers)}")
-        print(f"location: {response.location}")
-        print("==== END DEBUG response object ====")
+        
+        if VERBOSE:
+            print("==== DEBUG response object ====")
+            print("response object")
+            print(type(response))
+            print(dir(response))
+            print("Interesting attributes:")
+            print(f"content_encoding: {response.content_encoding}")
+            print(f"data (byte string): {response.data}")
+            print(f"data type: {type(response.data)}")
+            print(f"get_data: {response.get_data()}")
+            print(f"status_code: {response.status_code}")
+            print(f"status_code type: {type(response.status_code)}")
+            print(f"json: {response.json}")
+            print(f"get_json: {response.get_json()}")
+            print(f"headers: {response.headers}")
+            print(f"headers type: {type(response.headers)}")
+            print(f"headers attributes: {dir(response.headers)}")
+            print(f"location: {response.location}")
+            print("==== END DEBUG response object ====")
 
-        # perform cleanup
-        os.close(temp_db_fh)  # close the file
-        os.unlink(app.app.config["DATABASE"])  # remove the file
-
-        self.fail()
+        self.assertEqual(response.status_code, 200)
 
 
+    def test_my_todos_renders_correct_template(self):
+        response = self.app.get('/')
+
+        expected_text = b'<title>Todo API with Flask</title>'
+        self.assertIn(expected_text, response.data)
 
 
 # ------------------------
