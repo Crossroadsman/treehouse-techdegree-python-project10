fd7a3b7417b00a09e7973d91f6cf05ab9623ee89
CrossroadsMan <crossroadsman@gmail.com>
2019-01-18 11:12:17 -0700

    create additional fields on model

--- todo/models.py
+++ todo/models.py
@@ -14,6 +14,11 @@ DATABASE = SqliteDatabase('todo.sqlite')
 class Todo(Model):
     name = CharField()
     created_date = DateTimeField(default=datetime.datetime.now)
+    complete = BooleanField(default=False)
+    
+    # do we really need this field? It seems to be used just on the Angular
+    # side to represent changed since last database read.
+    edited = BooleanField(default=False)
 
     class Meta:
         database = DATABASE
