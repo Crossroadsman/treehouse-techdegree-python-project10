024123cae0d5ea6eff763f1c04670695bf93b3d6
CrossroadsMan <crossroadsman@gmail.com>
2019-01-18 16:19:27 -0700

    Implement favicon route

--- todo/app.py
+++ todo/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, g, jsonify, render_template
+import os
+
+from flask import Flask, g, jsonify, render_template, send_from_directory
 
 import config
 import models
@@ -12,6 +14,16 @@ app.register_blueprint(todos_api, url_prefix=config.API_URL_PREFIX)
 @app.route('/')
 def my_todos():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    """This is to get rid of the annoying 404 for the favicon"""
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'  # this mimetype per flask docs
+    )
 
 
 # ----------------
